6df4e9b190aa2a8bfc211681e2e4bc54d79d1f07
deviantdes <des-@live.com.sg>
2017-01-12 23:14:09 +0800

    fix read

--- GitApp/GitApp/Program.cs
+++ GitApp/GitApp/Program.cs
@@ -12,7 +12,7 @@ namespace GitApp
         {
             Console.Write("123");
             Console.Write("test print");
-            Console.read();
+            Console.Read();
         }
     }
 }
